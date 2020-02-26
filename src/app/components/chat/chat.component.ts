1ebd5e6471d9e336fbb45c3bd5220a8f0436e2db
Julian David Echeverri Garcia <jeheverri@PASCUALBRAVO.CENTRAL>
2020-02-26 13:57:05 -0500

    Improving chat behavior

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../providers/chat.service';
 
 @Component({
@@ -6,23 +6,34 @@ import { ChatService } from '../../providers/chat.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
   mensaje = '';
+  // se crea una variable elemento para realizar el focus cuando se agrega un mensaje
+  elemento: any;
   // inyectamos el proveedor - servicio
   constructor(public chatService: ChatService) {
     // nos susbscribimos al observable que nos devuelve la funcion de cargar mensajes
     // chatService.cargarMensajes().subscribe((mensajes: any[]) => {
     //   console.log(mensajes);
     // });
-    chatService.cargarMensajes().subscribe();
+
+    // apenas sean cargados los mensajes, se realiza la accion del focus
+    chatService.cargarMensajes().subscribe(() => {
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      }, 20);
+    });
   }
 
+  ngOnInit() {
+    this.elemento = document.getElementById("app-mensajes");
+  }
 
   enviar_mensaje() {
     if (this.mensaje.length === 0) return;
     this.chatService.agregarMensaje(this.mensaje)
-    .then(() => this.mensaje = "")
-    .catch((err) => console.error('Error al enviar', err)); // recibo el error de firebase
+      .then(() => this.mensaje = "")
+      .catch((err) => console.error('Error al enviar', err)); // recibo el error de firebase
     // console.log(this.mensaje);
   }
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -20,13 +20,22 @@ export class ChatService {
   // no se realiza en el constructor pues se inyectara el servicio cuando sea necesario
   cargarMensajes() {
     // se obtienen los datos y se subscriben a un observable para tomar desiciones
-    this.itemsCollection = this.afs.collection<Mensaje>('chats');
+    // importante: la insercion a firebase chat se mostrara en desorden. para ordenarla se debe realizar una query a firbase
+    // se manda una query, collection recibe otro parametro que es la referencia, query function
+    // forma de aplicar query a la collecion de firebase ---> ref => ref.orderBy('fecha', 'asc')
+    // con limit le digo que solo me devuelta 5
+    this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy('fecha', 'desc'));//.limit(5));
 
     // devuelvo el observable al cual me subscribire en donde lo llame
     return this.itemsCollection.valueChanges().pipe(
         map((mensajes: Mensaje[]) =>{
           console.log(mensajes);
-          this.chats = mensajes;
+          // this.chats = mensajes;
+          this.chats = [];
+          // se realizo nuevamente el array para mostrarlo en orden
+          for (let mensaje of mensajes) {
+            this.chats.unshift(mensaje);
+          }  
         })
       );
   }
@@ -34,12 +43,13 @@ export class ChatService {
   agregarMensaje(texto: string){
     // creamos un objeto del tipo de nuestra interface
     let mensaje: Mensaje = {
-      nombre: 'Demo',
-      mensaje: texto,
-      fecha: new Date().getTime()      
+        nombre: 'Demo',
+        mensaje: texto,
+        fecha: new Date().getTime()      
     }
    // lo añadimos a nuestra coleccion de firebase la cual regresa un promise
    // si se inserta hace el metodo then, sino el catch
     return this.itemsCollection.add(mensaje);
+
   }
 }
